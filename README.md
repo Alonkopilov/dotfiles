3826ef77a3bf4076e9eae52045bda1179673f398
Alon Kopilov <alonkop12@gmail.com>
2022-12-07 22:46:38 +0200

    Initial upload

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# dotfiles
-My Desktop Linux Configuration.
+# i3 .dotfiles
+
+My current Linux desktop workflow,
+Feel free to look around.
+
+Have a question? contact me at _alonkop12@gmail.com_
+
+## Preview
+![Cover Image](assets/showcase.png?raw=true)
+
+## Author
+[@Alon Kopilov](https://github.com/Alonkopilov) _alonkop12@gmail.com_
---
+++ init.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Create links
+ln -fs ~/dotfiles/.bashrc ~
+ln -fs ~/dotfiles/.fonts/ ~
+ln -fs ~/dotfiles/.spicetify/ ~
+
+ln -fs ~/dotfiles/.config/i3/ ~/.config/
+ln -fs ~/dotfiles/.config/neofetch/ ~/.config/
+ln -fs ~/dotfiles/.config/picom/ ~/.config/
+ln -fs ~/dotfiles/.config/polybar/ ~/.config/
+ln -fs ~/dotfiles/.config/rofi/ ~/.config/
+ln -fs ~/dotfiles/.config/spicetify/ ~/.config/
